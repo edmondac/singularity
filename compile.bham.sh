18022c4b1b7d11e681fbe119f24044d746aeafcc
edmondac-admin <a.c.edmondson@bham.ac.uk>
2017-07-07 12:35:53 +0100

    Compile singularity for BlueBEAR

---
+++ compile.bham.sh
@@ -0,0 +1,13 @@
+set -e
+set -x
+
+module purge
+module load bluebear
+#module load Autoconf/2.69-foss-2016.10
+module load Autotools/20150215-foss-2016.10
+
+./autogen.sh
+./configure
+make dist
+rpmbuild -ta singularity-*.tar.gz
+
